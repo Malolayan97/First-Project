7ed247fce20440fdefa3cc9f766632f6e04ccee6
Malolayan97 <m.alolayan97@hmail.com>
2025-01-10 22:00:18 +0300

    First project

---
+++ app.js
@@ -0,0 +1,22 @@
+// اول شي بنعرف الارراي
+let array = ["Cadillac","Honda","Mercedes","Audi"];
+
+//لما نستخدم بوش بينضاف عنصر جديد على الارراي 
+array.push("Porsche");
+console.log(array);
+
+//لما نستخدم بوب بينحذف اخر عنصر بالارراي 
+array.pop();
+console.log(array);
+
+// لما نستخدم انشفت بينضاف عنصر باول الارراي
+array.unshift("BMW");
+console.log(array);
+
+//لما نستخدم شفت بينحذف العنصر الاول من الارراي 
+array.shift();
+console.log(array);
+
+//لما نستخدم ريفيرس بنعكس ترتيب الارراي
+array.reverse();
+console.log(array);
